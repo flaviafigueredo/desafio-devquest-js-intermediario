d57ed0eb667eb9c1773cb5af4ebb18244d5b0244
Flávia Figueredo <flaviagfigueredo@gmail.com>
2022-11-28 11:56:23 -0300

    refatoração do código e implementação do forEach

--- src/js/index.js
+++ src/js/index.js
@@ -1,47 +1,20 @@
 const botao = document.getElementById("btn")
+const campos = document.querySelectorAll("#campo")
+const obrigatorio = document.getElementsByClassName("obrigatorio")
 
-botao.addEventListener("click", validacao)
+botao.addEventListener("click", function (event) {
+    event.preventDefault()
+    campos.forEach(item => {
+        item.classList.remove("valido")
 
-function validacao(evento) {
-    evento.preventDefault()
-
-    const nome = document.querySelector("#nome")
-    const obrigatorioNome = document.querySelector(".obrigatorio-nome") 
-    if (!nome.value) {               
-        obrigatorioNome.classList.add("visivel")
-        nome.classList.add("invalido")
-    } else {        
-        obrigatorioNome.classList.remove("visivel")
-        nome.classList.add("valido")
-    }
-
-    const email = document.querySelector("#email")
-    const obrigatorioEmail = document.querySelector(".obrigatorio-email")
-    if (!email.value) {        
-        obrigatorioEmail.classList.add("visivel")
-        email.classList.add("invalido")
-    } else {
-        obrigatorioEmail.classList.remove("visivel")
-        email.classList.add("valido")
-    }
-
-    const telefone = document.querySelector("#telefone")
-    const obrigatorioTelefone = document.querySelector(".obrigatorio-telefone")
-    if (!telefone.value) {        
-        obrigatorioTelefone.classList.add("visivel")
-        telefone.classList.add("invalido")
-    } else {
-        obrigatorioTelefone.classList.remove("visivel")
-        telefone.classList.add("valido")
-    }
-
-    const mensagem = document.querySelector("#mensagem")
-    const obrigatorioMensagem = document.querySelector(".obrigatorio-mensagem")
-    if (!mensagem.value) {        
-        obrigatorioMensagem.classList.add("visivel")
-        mensagem.classList.add("invalido")
-    } else {
-        obrigatorioMensagem.classList.remove("visivel")
-        mensagem.classList.add("valido")
-    }
-}
+        for (let i = 0; i < campos.length; i++) {
+            if (!campos[i].value) {
+                campos[i].classList.add("invalido")
+                obrigatorio[i].classList.add("visivel")
+            } else {
+                obrigatorio[i].classList.remove("visivel")
+                campos[i].classList.add("valido")
+            }
+        }
+    })
+})
